f38d2628f3833219a8efbc09de662377a6bebba8
Pedram Khabbazian <p.khabbazian@77agency.com>
2022-11-24 13:44:10 +0100

    edit README.md file add first project.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 100 Python Projects
 **100 python projects** from beginner to pro.
 
+## Contents:
+    1.Band Name Generator
+
+
---
+++ projects/1_band_name_generator.py
@@ -0,0 +1,5 @@
+# Welcome to the Band Name Generator!
+
+city = input("Which city you grew up in? \n")
+pet = input("What's your pet name? \n")
+print("Your band name could be " + city + " " + pet)
